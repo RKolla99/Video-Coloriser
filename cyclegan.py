8a3e22da20f312c13007c4f5326b06fb1b81d737
Sriram <sriramsk1999@gmail.com>
2020-04-18 20:01:29 +0530

    Added 3D versions of all functions

--- cyclegan.py
+++ cyclegan.py
@@ -2,14 +2,68 @@ import warnings
 with warnings.catch_warnings():
   warnings.filterwarnings("ignore",category=FutureWarning)
   import tensorflow as tf
-from model import CycleGAN
+from model import CycleGAN, CycleGAN1
 from data_loader import get_data
+import numpy as np
+import cv2
+import ops
 
-print('Read data:')
-train_A, train_B, test_A, test_B = get_data('apple2orange', 128)
+#print('Read data:')
+#train_A, train_B, test_A, test_B = get_data('apple2orange', 128)
+
+cap = cv2.VideoCapture('movie.mp4')
+frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+fc=0
+ret=True
+buf = np.empty((12, 256, 256, 3), np.dtype('uint8'))
+bnw = np.empty((12, 256, 256), np.dtype('uint8'))
+
+while (fc < frameCount  and ret):
+    ret, frame = cap.read()
+    buf[fc] = cv2.resize(frame,(256,256))
+    bnw[fc] = cv2.cvtColor(buf[fc], cv2.COLOR_BGR2GRAY)
+    fc += 1
+    if (fc==12):
+    	break
+cap.release()
+
+'''
+# Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
+# Define the fps to be equal to 10. Also frame size is passed.
+out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc(*'MJPG'), 30, (256,256))
+for i in range(len(buf)):
+	out.write(buf[i])
+out.release()
+'''
+bnw = np.expand_dims(bnw,axis=3)
+bnw = np.expand_dims(bnw,axis=0)
+buf = np.expand_dims(buf,axis=0)
+print(np.shape(buf))
+
+'''
+#colour to black and white
+G_ab = Generator1('G_ab', is_train=True, 
+                 norm='instance', activation='relu', image_size=256)
+G_ba = Generator2('G_ba', is_train=True, 
+                 norm='instance', activation='relu', image_size=256)
+
+fake_buf = G_ab(tf.convert_to_tensor(buf,dtype=tf.float32))
+cycle_buf = G_ba(tf.convert_to_tensor(fake_buf,dtype=tf.float32))
+
+D_a = Discriminator('D_a', is_train=True,    #input 1, output 3 channel
+                    norm='instance', activation='leaky')
+D_b = Discriminator('D_b', is_train=True, #input 3, output 1 channel
+                    norm='instance', activation='leaky')
+
+D_real_a = D_a(tf.convert_to_tensor(fake_buf,dtype=tf.float32))
+D_real_b = D_b(tf.convert_to_tensor(cycle_buf,dtype=tf.float32))
+'''
 
 print('Build graph:')
-model = CycleGAN()
+model = CycleGAN1()
 
 variables_to_save = tf.compat.v1.global_variables()
 init_op = tf.compat.v1.variables_initializer(variables_to_save)
@@ -20,4 +74,4 @@ with tf.compat.v1.Session() as sess:
     sess.run(init_all_op)
 
     print("Starting training session.")
-    model.train(sess, train_A, train_B)
+    model.train(sess, buf, bnw)
--- ops.py
+++ ops.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+import numpy as np
 
 def _norm(input, is_train, reuse=True, norm=None):
     assert norm in ['instance', 'batch', None]
@@ -98,3 +98,67 @@ def deconv_block(input, num_filters, name, k_size, stride, is_train, reuse, norm
         out = _norm(out, is_train, reuse, norm)
         out = _activation(out, activation)
         return out
+
+
+
+
+
+
+def conv3_block(input, num_filters, name, k_size, stride, is_train, reuse, norm,
+          activation, pad='SAME', bias=False):
+    with tf.compat.v1.variable_scope(name, reuse=reuse):
+        out = conv3d(input, num_filters, k_size, stride, reuse, pad, bias=bias)
+        out = _norm(out, is_train, reuse, norm)
+        out = _activation(out, activation)
+        return out
+
+def residual3(input, num_filters, name, is_train, reuse, norm, pad='REFLECT'):
+    with tf.compat.v1.variable_scope(name, reuse=reuse):
+        with tf.compat.v1.variable_scope('res1', reuse=reuse):
+            out = conv3d(input, num_filters, 3, 1, reuse, pad)
+            out = _norm(out, is_train, reuse, norm)
+            out = tf.nn.relu(out)
+
+        with tf.compat.v1.variable_scope('res2', reuse=reuse):
+            out = conv3d(out, num_filters, 3, 1, reuse, pad)
+            out = _norm(out, is_train, reuse, norm)
+
+        return tf.nn.relu(input + out)
+
+def deconv3_block(input, num_filters, name, k_size, stride, is_train, reuse, norm, activation):
+    with tf.compat.v1.variable_scope(name, reuse=reuse):
+        out = conv3d_transpose(input, num_filters, k_size, stride, reuse)
+        out = _norm(out, is_train, reuse, norm)
+        out = _activation(out, activation)
+        return out
+
+def conv3d(input, num_filters, filter_size, stride, reuse=False,
+           pad='SAME', dtype=tf.float32, bias=False):
+    stride_shape = [1, stride, stride, stride, 1]
+    filter_shape = [filter_size, filter_size, filter_size, input.get_shape()[4], num_filters]
+
+    w = tf.compat.v1.get_variable('w', filter_shape, dtype, tf.random_normal_initializer(0.0, 0.02))
+    if pad == 'REFLECT':
+        p = (filter_size - 1) // 2
+        x = tf.pad(input, [[0,0],[p,p],[p,p],[p,p],[0,0]], 'REFLECT')
+        conv = tf.nn.conv3d(x, w, stride_shape, padding='VALID')
+    else:
+        assert pad in ['SAME', 'VALID']
+        conv = tf.nn.conv3d(input, w, stride_shape, padding=pad)
+
+    if bias:
+        b = tf.compat.v1.get_variable('b', [1,1,1,num_filters], initializer=tf.constant_initializer(0.0))
+        conv = conv + b
+    return conv
+
+def conv3d_transpose(input, num_filters, filter_size, stride, reuse,
+                     pad='SAME', dtype=tf.float32):
+    assert pad == 'SAME'
+    n, d, h, w, c = input.get_shape().as_list()
+    stride_shape = [1, stride, stride, stride, 1]
+    filter_shape = [filter_size, filter_size, filter_size, num_filters, c]
+    output_shape = [n, d * stride,h * stride, w * stride, num_filters]
+
+    w = tf.compat.v1.get_variable('w', filter_shape, dtype, tf.random_normal_initializer(0.0, 0.02))
+    deconv = tf.nn.conv3d_transpose(input, w, output_shape, stride_shape, pad)
+    return deconv
